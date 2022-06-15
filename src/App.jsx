721b1bc8f2f8372863414ef361d9ba89b826545a
Orleydovsky <orleydovsky@gmail.com>
2022-06-15 08:34:45 -0500

    Deletion of default App vite creates
    App.jsx no longer renders default vite App
    -General layout added
    -must change estaFuncion to a more descriptive name

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,70 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Dialog } from "@reach/dialog";
+import '@reach/dialog/styles.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Form({estaFuncion, buttonText}) {
+    const handleSubmit = e => {
+        e.preventDefault()
+        const {username, password} = e.target.elements
+        estaFuncion({
+            username: username.value,
+            password: password.value
+        })
+    }
+    return (
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="username">Username:</label> 
+            <input id="username" type="text" />
+        </div>
+        <div>
+            <label id="password" htmlFor="password">Password:</label> 
+            <input id="password" type="password" />
+        </div>
+        <button type="submit">{buttonText}</button>
+    </form>
+    )
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+export default function App() {
+    const fromLogin = (thisFormData) => {
+        console.log('loginData', thisFormData)
+    }
+    const fromRegister = (thisFormData) => {
+        console.log('registerData', thisFormData)
+    }
+    const [openModal, setOpenModal] = useState({
+        login: false,
+        register: false,
+    })
+    const {login, register} = openModal
+    const handleClick = e => {
+        setOpenModal({
+            ...openModal,
+            [e.target.name]: true,
+        })
+    }
+    const handleClosing = () => {
+        setOpenModal({login: false, register: false})
+    }
+
+    return <>
+        <h1>Bookshelf</h1>
+        <button name="login" onClick={handleClick}>Login</button>
+        <button name="register" onClick={handleClick}>Register</button>
+
+        <Dialog aria-label="Login form" isOpen={login}>
+        <h3>Login content</h3>
+            <button onClick={handleClosing}>&#10005;</button>
+            <Form estaFuncion={fromLogin} buttonText="Login"/>
+        </Dialog>
+        <Dialog aria-label="Registration form" isOpen={register}>
+        <h3>Register content</h3>
+            <button onClick={handleClosing}>&#10005;</button>
+            <Form estaFuncion={fromRegister}  buttonText="Register" />
+        </Dialog>
+    </>
 }
-
-export default App
+const root = createRoot(document.getElementById("root"))
+root.render(<App />)
