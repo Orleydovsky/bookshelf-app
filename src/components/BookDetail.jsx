eba10ef61326358efcf2af1ee3b07599618341e7
Orleydovsky <orleydovsky@gmail.com>
2022-06-23 21:43:52 -0500

    Improved BookDetail syles and added book image

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from "react-router-dom";
-import Card from './Card';
+import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 import { client } from '../utils/client';
-import parse from 'html-react-parser';
+
 
 
 
@@ -26,24 +25,36 @@ function BookDetail() {
         <div css={{
 
             width: '60vw',
-            backgroundColor: '#DDD',
+            // backgroundColor: '#DDD',
             marginTop: '15px',
             borderRadius: '15px',
-            boxShadow: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',
+            // boxShadow: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',
             padding: '15px',
             }}>
             <h2>{data?.volumeInfo.title} | {data?.volumeInfo.authors}</h2>
             <h3>{data?.volumeInfo.subtitle}</h3>
+            <div css={{
+                boxShadow: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',
+                borderRadius: '15px',
+                width: '150px',
+                height: '200px',
+                backgroundColor: 'white',
+                backgroundImage: `url('${data.volumeInfo.imageLinks?.thumbnail}')`,
+                backgroundSize: 'cover',
+                }}>
+
+            </div>
         <div css={{
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
         }}>
             <div css={{
-                backgroundColor: 'white',
+                // backgroundColor: 'white',
                 borderRadius: '15px',
                 marginTop:' 15px',
                 padding: '15px',
+                color: 'black',
              }} dangerouslySetInnerHTML={{__html: data?.volumeInfo.description}}>
             </div>
         </div>
